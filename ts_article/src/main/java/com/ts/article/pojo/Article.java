fcab6a4d60ce39279f60d8a32bdd6f8e94c2e703
SanJingYe88 <822202281@qq.com>
2019-03-25 13:57:36 +0800

    部分属性类型的修改

--- ts_article/src/main/java/com/ts/article/pojo/Article.java
+++ ts_article/src/main/java/com/ts/article/pojo/Article.java
@@ -11,6 +11,9 @@ import javax.persistence.Table;
 import java.io.Serializable;
 import java.util.Date;
 
+/**
+ * 文章实体类
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -18,7 +21,6 @@ import java.util.Date;
 @Entity
 @Table(name = "tb_article")
 public class Article implements Serializable {
-
     @Id
     private String id;//ID
     private String columnid;//专栏ID
@@ -28,13 +30,13 @@ public class Article implements Serializable {
     private String image;//文章封面
     private Date createtime;//发表日期
     private Date updatetime;//修改日期
-    private String ispublic;//是否公开 ( 0：不公开 1：公开 )
-    private String istop;//是否置顶 ( 0：不置顶 1：置顶 )
+    private Integer ispublic;//是否公开 ( 0：不公开 1：公开 )
+    private Integer istop;//是否置顶 ( 0：不置顶 1：置顶 )
     private Integer visits;//浏览量
     private Integer thumbup;//点赞数
     private Integer comment;//评论数
-    private String state;//审核状态 ( 0：未审核 1：已审核 )
+    private Integer state;//审核状态 ( 0：未审核 1：已审核 )
     private String channelid;//所属频道 ( 关联频道表ID )
     private String url;//URL
-    private String type;//类型 ( 0：分享 1：专栏 )
+    private Integer type;//类型 ( 0：分享 1：专栏 )
 }
