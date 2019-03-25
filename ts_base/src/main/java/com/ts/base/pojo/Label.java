0b18ba24d67078e46855e8187918e7e792f24abf
SanJingYe88 <822202281@qq.com>
2019-03-25 09:29:51 +0800

    实体属性类型修改

--- ts_base/src/main/java/com/ts/base/pojo/Label.java
+++ ts_base/src/main/java/com/ts/base/pojo/Label.java
@@ -5,9 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 @Data
@@ -20,9 +19,10 @@ public class Label implements Serializable{
 
     @Id
     private String id;          //标签ID
+    @NotNull(message = "标签名称不能为空")
     private String labelname;   //标签名称
-    private String state;       //状态 ( 0-无效 , 1-有效 )
+    private Integer state;       //状态 ( 0-无效 , 1-有效 )
     private Long count;         //使用数量
-    private String recommend;   //是否推荐 ( 0-不推荐 , 1-推荐 )
+    private Integer recommend;   //是否推荐 ( 0-不推荐 , 1-推荐 )
     private Long fans;          //粉丝数
 }
