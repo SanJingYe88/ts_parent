3fc5234cae62e0f467b95c078c3ff49d3588bf32
SanJingYe88 <822202281@qq.com>
2019-03-25 10:39:06 +0800

    Controller方法新增批量与修改

--- ts_base/src/main/java/com/ts/base/controller/LabelController.java
+++ ts_base/src/main/java/com/ts/base/controller/LabelController.java
@@ -8,15 +8,11 @@ import entity.StatusCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Semaphore;
 
 @Slf4j
 @RestController
@@ -27,114 +23,122 @@ public class LabelController {
     @Autowired
     LabelService labelService;
 
-    //标签全部列表
-    @GetMapping
-    public Result findAll() {
-        Result result = new Result(true, StatusCode.OK, "查询成功");
-        result.setData(labelService.findAll());
-        return result;
-    }
-
-    //根据ID查询
+    /**
+     * 根据ID查询
+     * @param id
+     * @return
+     */
     @GetMapping(value = "/{id}")
     public Result findById(@PathVariable String id) {
-        Result result = new Result(true, StatusCode.OK, "查询成功");
-        result.setData(labelService.findById(id));
-        log.info("result={}",result);
-        return result;
+        log.info("id={}",id);
+        return new Result(true, StatusCode.OK, "查询成功",labelService.findById(id));
     }
 
-    //增加标签
+    /**
+     * 增加标签
+     * @param label
+     * @return
+     */
     @PostMapping
-    public Result add(@RequestBody Label label) {
-        Result result = new Result(true, StatusCode.OK, "新增成功");
+    public Result add(@Validated @RequestBody Label label) {
+        log.info("label={}",label);
         labelService.add(label);
-        return result;
+        return new Result(true, StatusCode.OK, "新增成功");
     }
 
-    //推荐标签列表
+    /**
+     * 推荐标签列表
+     * @return
+     */
     @GetMapping(value = "/topList")
     public Result topList() {
-        Result result = new Result(true, StatusCode.OK, "查询成功");
-        Map map = new HashMap();
+        Map map = new HashMap(1);
         map.put("recommend", "1");
-        result.setData(labelService.findSearch(map));
-        return result;
+        return new Result(true, StatusCode.OK, "查询成功", labelService.findSearch(map));
     }
 
-    //有效标签列表
+    /**
+     * 有效标签列表
+     * @return
+     */
     @GetMapping(value = "/list")
     public Result list() {
-        Result result = new Result(true, StatusCode.OK, "查询成功");
-        Map map = new HashMap();
+        Map map = new HashMap(1);
         map.put("state", "1");
-        result.setData(labelService.findSearch(map));
-        return result;
+        return new Result(true, StatusCode.OK, "查询成功", labelService.findSearch(map));
     }
 
-    //修改标签
+    /**
+     * 修改标签
+     * @param id
+     * @param label
+     * @return
+     */
     @PutMapping(value = "/{id}")
     public Result update(@PathVariable String id, @RequestBody Label label) {
-        Result result = new Result(true, StatusCode.OK, "修改成功");
+        log.info("id={}",id);
+        log.info("label={}",label);
         label.setId(id);
         labelService.update(label);
-        return result;
+        return new Result(true, StatusCode.OK, "修改成功");
     }
 
-    //根据ID删除
+    /**
+     * 根据ID删除
+     * @param id
+     * @return
+     */
     @DeleteMapping(value = "/{id}")
     public Result delete(@PathVariable String id) {
-        Result result = new Result(true, StatusCode.OK, "删除成功");
+        log.info("id={}",id);
         labelService.delete(id);
-        return result;
+        return new Result(true, StatusCode.OK, "删除成功");
     }
 
-    //条件查询
-    @PostMapping(value = "/search")
-    public Result findSearch(@RequestBody Map map) {
-        Result result = new Result(true, StatusCode.OK, "查询成功");
-        result.setData(labelService.findSearch(map));
-        return result;
-    }
-
-    //条件查询 + 分页
+    /**
+     * 条件查询 + 分页
+     * @param page
+     * @param size
+     * @param searchMap
+     * @return
+     */
     @PostMapping(value = "/search/{page}/{size}")
-    public PageResult<Label> pageSearch(@PathVariable int page, @PathVariable int size, @RequestBody Map map) {
-        PageResult<Label> pageResult = new PageResult<>();
-        Page<Label> data = labelService.pageSearch(map, page, size);
-        pageResult.setRows(data.getContent());
-        pageResult.setTotal(data.getTotalElements());
-        return pageResult;
+    public Result pageSearch(@PathVariable() int page, @PathVariable int size, @RequestBody(required = false) Map searchMap) {
+        log.info("page={}",page);
+        log.info("size={}",size);
+        log.info("searchMap={}",searchMap);
+        Page<Label> data = labelService.pageSearch(searchMap, page, size);
+        PageResult<Label> pageResult = new PageResult<>(data.getTotalElements(),data.getContent());
+        return new Result(true, StatusCode.OK, "查询成功",pageResult);
     }
 
-    @GetMapping("/test")
-    public void test() throws InterruptedException {
-        log.info("{}","----------");
-        ExecutorService executorService = Executors.newCachedThreadPool();
-        CountDownLatch countDownLatch = new CountDownLatch(requestTotal);
-        Semaphore semaphore = new Semaphore(concurrentThreadNum);
-        for (int i = 0; i < requestTotal; i++) {
-            executorService.execute(() -> {
-                try {
-                    semaphore.acquire();
-                    Result result = restTemplate.getForObject("http://ts-base/label/1071765323784523776", Result.class);
-                    log.info("result:{}.", result.getData());
-                    semaphore.release();
-                } catch (InterruptedException e) {
-                    log.error("exception", e);
-                }
-                countDownLatch.countDown();
-            });
-        }
-        countDownLatch.await();
-        executorService.shutdown();
-        log.info("请求完成");
+    /**
+     * 批量删除
+     * @param map
+     * @return
+     */
+    @DeleteMapping(value = "/batchDelete")
+    public Result batchDelete(@RequestBody Map map) {
+        String ids = (String) map.get("ids");
+        log.info("ids={}",ids);
+        labelService.batchDelete(ids);
+        return new Result(true, StatusCode.OK, "删除成功");
     }
 
-    @Autowired
-    private RestTemplate restTemplate;
-    // 总的请求个数
-    private static final int requestTotal = 1000;
-    // 同一时刻最大的并发线程的个数
-    private static final int concurrentThreadNum = 20;
+    /**
+     * 批量修改
+     * @param map
+     * @return
+     */
+    @PutMapping(value = "/batchUpdate")
+    public Result batchUpdate(@RequestBody Map map) {
+        String ids = (String) map.get("ids");
+        String operateType = (String) map.get("operateType");
+        Integer newValue = (Integer) map.get("newValue");
+        log.info("ids={}",ids);
+        log.info("operateType={}",operateType);
+        log.info("newValue={}",newValue);
+        labelService.batchUpdate(ids,operateType,newValue);
+        return new Result(true, StatusCode.OK, "修改成功");
+    }
 }
