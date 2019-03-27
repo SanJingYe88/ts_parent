62ff50ea5434975952bb59768b9e6850fff21889
SanJingYe88 <822202281@qq.com>
2019-03-27 09:49:15 +0800

    添加了参数校验异常处理

--- ts_gathering/src/main/java/com/ts/gathering/controller/BaseExceptionHandler.java
+++ ts_gathering/src/main/java/com/ts/gathering/controller/BaseExceptionHandler.java
@@ -2,19 +2,25 @@ package com.ts.gathering.controller;
 
 import entity.Result;
 import entity.StatusCode;
+import exception.CheckException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * 统一异常处理类
  */
 @ControllerAdvice
 public class BaseExceptionHandler {
+
+    // 处理参数验证异常
+    @ExceptionHandler(value = CheckException.class)
+    @ResponseBody
+    public Result handleCheckException(CheckException e) {
+        e.printStackTrace();
+        String message = e.getMessage();
+        return new Result(false, StatusCode.PARAM_ERROR, message);
+    }
 
     @ExceptionHandler(value = Exception.class)
     @ResponseBody
