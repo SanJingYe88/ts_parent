2675fc39fdccbe1ff79f95a9b7e27d46783c6554
SanJingYe88 <822202281@qq.com>
2019-03-27 09:46:29 +0800

    去掉无用方法,添加了日志输出

--- ts_gathering/src/main/java/com/ts/gathering/controller/GatheringController.java
+++ ts_gathering/src/main/java/com/ts/gathering/controller/GatheringController.java
@@ -2,6 +2,7 @@ package com.ts.gathering.controller;
 
 import java.util.Map;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -19,8 +20,9 @@ import entity.Result;
 import entity.StatusCode;
 
 /**
- * 控制器层
+ * Gathering 控制器层
  */
+@Slf4j
 @RestController
 @CrossOrigin
 @RequestMapping("/gathering")
@@ -29,32 +31,19 @@ public class GatheringController {
     @Autowired
     private GatheringService gatheringService;
 
-
-    /**
-     * 查询全部数据
-     *
-     * @return
-     */
-    @RequestMapping(method = RequestMethod.GET)
-    public Result findAll() {
-        return new Result(true, StatusCode.OK, "查询成功", gatheringService.findAll());
-    }
-
     /**
      * 根据ID查询
-     *
      * @param id ID
      * @return
      */
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public Result findById(@PathVariable String id) {
+        log.info("id={}",id);
         return new Result(true, StatusCode.OK, "查询成功", gatheringService.findById(id));
     }
 
-
     /**
      * 分页+多条件查询
-     *
      * @param searchMap 查询条件封装
      * @param page      页码
      * @param size      页大小
@@ -62,39 +51,33 @@ public class GatheringController {
      */
     @RequestMapping(value = "/search/{page}/{size}", method = RequestMethod.POST)
     public Result findSearch(@RequestBody Map searchMap, @PathVariable int page, @PathVariable int size) {
+        log.info("page={}",page);
+        log.info("size={}",size);
+        log.info("searchMap={}",searchMap);
         Page<Gathering> pageList = gatheringService.findSearch(searchMap, page, size);
-        return new Result(true, StatusCode.OK, "查询成功", new PageResult<Gathering>(pageList.getTotalElements(), pageList.getContent()));
-    }
-
-    /**
-     * 根据条件查询
-     *
-     * @param searchMap
-     * @return
-     */
-    @RequestMapping(value = "/search", method = RequestMethod.POST)
-    public Result findSearch(@RequestBody Map searchMap) {
-        return new Result(true, StatusCode.OK, "查询成功", gatheringService.findSearch(searchMap));
+        PageResult<Gathering> pageResult = new PageResult<>(pageList.getTotalElements(), pageList.getContent());
+        return new Result(true, StatusCode.OK, "查询成功", pageResult);
     }
 
     /**
      * 增加
-     *
      * @param gathering
      */
     @RequestMapping(method = RequestMethod.POST)
     public Result add(@RequestBody Gathering gathering) {
+        log.info("gathering={}",gathering);
         gatheringService.add(gathering);
         return new Result(true, StatusCode.OK, "增加成功");
     }
 
     /**
      * 修改
-     *
      * @param gathering
      */
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     public Result update(@RequestBody Gathering gathering, @PathVariable String id) {
+        log.info("id={}",id);
+        log.info("gathering={}",gathering);
         gathering.setId(id);
         gatheringService.update(gathering);
         return new Result(true, StatusCode.OK, "修改成功");
@@ -102,13 +85,12 @@ public class GatheringController {
 
     /**
      * 删除
-     *
      * @param id
      */
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    @RequestMapping(value = "/show/{id}", method = RequestMethod.DELETE)
     public Result delete(@PathVariable String id) {
-        gatheringService.deleteById(id);
+        log.info("id={}",id);
+        gatheringService.updateShowById(id);
         return new Result(true, StatusCode.OK, "删除成功");
     }
-
 }
