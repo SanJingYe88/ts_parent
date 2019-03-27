8091db0843e6ffbcc94e02a8fd347878a5cf9c62
SanJingYe88 <822202281@qq.com>
2019-03-27 09:50:06 +0800

    添加了新属性,及其Dao方法

--- ts_gathering/src/main/java/com/ts/gathering/dao/GatheringDao.java
+++ ts_gathering/src/main/java/com/ts/gathering/dao/GatheringDao.java
@@ -4,10 +4,21 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.ts.gathering.pojo.Gathering;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * 数据访问接口
  */
 public interface GatheringDao extends JpaRepository<Gathering, String>, JpaSpecificationExecutor<Gathering> {
 
+    /**
+     * 设置活动是否显示
+     * @param id
+     */
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true,value = "UPDATE tb_gathering SET canshow = CASE WHEN canshow = 1 THEN 0 ELSE 1 END WHERE id = ?1")
+    void updateShowById(String id);
 }
--- ts_gathering/src/main/java/com/ts/gathering/pojo/Gathering.java
+++ ts_gathering/src/main/java/com/ts/gathering/pojo/Gathering.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -12,9 +13,8 @@ import java.io.Serializable;
 import java.util.Date;
 
 /**
- * 活动类
+ * Gathering 活动实体类
  */
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -24,16 +24,21 @@ import java.util.Date;
 public class Gathering implements Serializable {
 
     @Id
-    private String id;              //编号
+    private String id;              //ID
     private String name;            //活动名称
-    private String summary;         //大会简介
+    private String summary;         //活动简介
     private String detail;          //详细说明
     private String sponsor;         //主办方
     private String image;           //活动图片
-    private Date starttime;         //开始时间
-    private Date endtime;           //截止时间
-    private String address;         //举办地点
-    private Date enrolltime;        //报名截止
-    private String state;           //是否可见
+    @Column(name = "start_time")
+    private Date startTime;         //开始时间
+    @Column(name = "end_time")
+    private Date endTime;           //截止时间
+    @Column(name = "enroll_time")
+    private Date enrollTime;        //报名截止时间
     private String city;            //城市
+    private String address;         //举办地点
+    private Integer state;           //活动状态,(0-待审核,1-审核通过,2-审核不通过)
+    @Column(name = "canshow")
+    private Integer canShow;        //是否可见,(0-不可见,1-可见)
 }
