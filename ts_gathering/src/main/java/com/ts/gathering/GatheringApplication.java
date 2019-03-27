130863cbe814c9c281f06fd10c4cfc896bf38bcd
SanJingYe88 <822202281@qq.com>
2019-03-27 09:40:15 +0800

    将子工程Gathering改造为EurekaClient

--- ts_gathering/src/main/java/com/ts/gathering/GatheringApplication.java
+++ ts_gathering/src/main/java/com/ts/gathering/GatheringApplication.java
@@ -2,14 +2,16 @@ package com.ts.gathering;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import util.IdWorker;
 
+@EnableEurekaClient
 @SpringBootApplication
-public class Application {
+public class GatheringApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(GatheringApplication.class, args);
     }
 
     @Bean
